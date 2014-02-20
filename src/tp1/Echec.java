0f1632fbcd274e8fbc089bf9432b1c1256fb1945
Anthony Gelibert <anthony.gelibert@me.com>
2014-02-20 23:23:00 +0100

    Add TODO (Echec.java).

--- src/tp1/Echec.java
+++ src/tp1/Echec.java
@@ -3,13 +3,11 @@ package tp1;
 
 public class Echec
 {
+    /* TODO Qu'est-ce ? */
     public int x;
-
+    /* TODO Qu'est-ce ? */
     public int y;
-
-    public int Case[][] =
-    {
-    { 1, 2, 3, 4, 5, 6, 7, 8 },
-    { 1, 2, 3, 4, 5, 6, 7, 8 } };
+    /* TODO Qu'est-ce ? */
+    public int Case[][] = {{1, 2, 3, 4, 5, 6, 7, 8}, {1, 2, 3, 4, 5, 6, 7, 8}};
 
 }
