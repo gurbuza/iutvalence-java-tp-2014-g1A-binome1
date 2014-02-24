12c81397ef51fa138ae88056c03c0215f8bc806b
gurbuza <adem.gurbuz@iut-valence.fr>
2014-02-24 16:51:08 +0100

    Premiere étape de Pion.java

--- src/tp1/Pion.java
+++ src/tp1/Pion.java
@@ -3,21 +3,18 @@ package tp1;
 
 public class Pion
 {
-    /* TODO Pourquoi une chaine ? Est-ce que le nombre de solutions n'est pas connu ? */
-    public String couleur;
-    /* TODO Pourquoi une chaine ? Est-ce que le nombre de solutions n'est pas connu ? */
-    /* TODO Notion d'héritage ? */
-    public String element;
-    /* TODO Qu'est-ce qu'une case ? */
-    private Case Position;
-    /* TODO Pourquoi un constructeur vide ? */
-    public Pion()
+    private final boolean couleur;
+    
+    public Pion(boolean couleur)
     {
+        this.couleur = couleur;
+    }
 
-    }
-    /* TODO Le pion peut se déplacer n'importe où ? */
-    public void avancerPion(Case p)
+
+    public void avancerPion()
     {
-        this.Position = p;
+        
     }
+
+    
 }
