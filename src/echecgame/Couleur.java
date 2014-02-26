4ec411660d033c69ed67d94ac72b9a1f06711de8
Anthony Gelibert <anthony.gelibert@me.com>
2014-02-26 22:02:55 +0100

    TODO Javadoc (Couleur.java).

--- src/echecgame/Couleur.java
+++ src/echecgame/Couleur.java
@@ -1,5 +1,9 @@
+/* TODO Javadoc. */
+/* TODO Nom de package. */
+
 package echecgame;
 
+/* TODO Pourquoi un "boolean" ? Pourquoi pas un enum ? */
 public class Couleur
 {
     public final static boolean BLANC = true;
