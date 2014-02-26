d0a15eae56cfd1a518c9d07aa3c79fedb0519901
Anthony Gelibert <anthony.gelibert@me.com>
2014-02-26 22:02:43 +0100

    TODO Javadoc (Case.java).

--- src/echecgame/Case.java
+++ src/echecgame/Case.java
@@ -1,11 +1,11 @@
 /* TODO Javadoc. */
+/* TODO Nom de package. */
 package echecgame;
 
 public class Case
 {
-    
     public Pion pion;
-
+    /* À quoi sert de retenir la couleur de la case ? */
     private final boolean couleur;
 
     public Case(boolean couleur)
@@ -14,6 +14,7 @@ public class Case
         this.couleur = couleur;
     }
 
+    /* TODO Evitez le franglais ! */
     public boolean getCouleur()
     {
         return couleur;
