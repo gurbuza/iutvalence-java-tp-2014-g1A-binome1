6dd625876543a740517c800cca6c9d348f018243
Anthony Gelibert <anthony.gelibert@me.com>
2014-02-26 22:03:52 +0100

    Add TODO (Pion.java).

--- src/echecgame/Pion.java
+++ src/echecgame/Pion.java
@@ -1,6 +1,8 @@
 /* TODO Javadoc. */
+/* TODO Nom de package. */
 package echecgame;
 
+/* TODO Allez vous manipuler des Pion ? Ou allez vous manipuler seulement les sous-classes ? */
 public class Pion
 {
     private final boolean couleur;
@@ -10,16 +12,14 @@ public class Pion
         this.couleur = couleur;
     }
 
-
     public boolean getCouleur()
     {
         return couleur;
     }
-    
+
+    /* TODO Est-ce une méthode pertinente ? */
     public void avancerPion()
     {
-        /*A completer*/
+        /* Votre commentaire : A completer. Le mien :   TODO Est-ce réellement nécessaire  ? */
     }
-
-    
 }
