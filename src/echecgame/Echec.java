01b2a1141f96f937ed7f0c4235510892575872d8
Anthony Gelibert <anthony.gelibert@me.com>
2014-03-14 11:06:56 +0100

    Add TODO (Echec.java).

--- src/echecgame/Echec.java
+++ src/echecgame/Echec.java
@@ -1,20 +1,20 @@
 /* TODO Javadoc. */
-/* TODO Nom de package. */
+/* TODO Nom de package incorrect. À discuter en TP. */
 package echecgame;
 
 public class Echec
 {
-    private final Pion echiquier[][];
+    private final Pion[][] echiquier;
 
     private final Joueur joueur1;
 
     private final Joueur joueur2;
-    
+
     private boolean joueurCourant;
 
-    public Echec()
-    {
-       this.echiquier = new Pion[8][8];
+    public Echec() {
+       echiquier = new Pion[8][8];
+       /* TODO Mettre ce placement dans une méthode dédiée pour faciliter la lecture. */
        /* Placement des pions. */
        echiquier[0][0] = new PionTour(Couleur.BLANC);
        echiquier[0][1] = new PionCavalier(Couleur.BLANC);
@@ -36,7 +36,7 @@ public class Echec
        echiquier[7][7] = new PionTour(Couleur.NOIR);
        for (int colonne=0;colonne<8; colonne++)
            echiquier[6][colonne] = new PionSoldat(Couleur.NOIR);
-       
+
        this.joueur1 = new Joueur(Couleur.BLANC, "Tom");
        this.joueur2 = new Joueur(Couleur.NOIR, "Jerry");
        this.joueurCourant = true;
@@ -55,6 +55,7 @@ public class Echec
        System.out.println(echiquierAsciiArt);
     }
 
+    /* TODO Ne devrait pas être "static". */
     public static void jouer()
     {
         /* Algo d'un tour :
